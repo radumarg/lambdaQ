f5a88222a76c446602b26d48b4b4e68ca7bebb48
Radu Marginean <radu.marg@gmail.com>
2023-07-17 13:57:48 +0300

    Update SemanticAnalyser.hs

--- src/Backend/SemanticAnalyser.hs
+++ src/Backend/SemanticAnalyser.hs
@@ -1,7 +1,9 @@
-module Backend.SemanticAnalyer where
-
   -- TODO:
   -- function names are unique
   -- no duplicate function declaration exists
   -- function signatures and definitions have matching names
-  -- the number of function arguments match the arguments in the signature 
+  -- the number of function arguments match the arguments in the signature
+  -- control qubits for controlled gates are different from target qubit
+
+module Backend.SemanticAnalyer where
+
