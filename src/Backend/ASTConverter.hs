e136795caac7f9eefca3fabde3a80d4b1a701fe9
Radu Marginean <radu.marg@gmail.com>
2023-07-20 13:27:46 +0300

    Some work on language backend.

--- src/Backend/ASTConverter.hs
+++ src/Backend/ASTConverter.hs
@@ -3,7 +3,7 @@ module Backend.ASTConverter where
 -- A representation of a lambdaQ program in the shape of an abstract syntax tree
 -- will be generated here. Subsequently the abstract syntax tree is converted
 -- to an intermediate abstract syntax tree with a simpler syntax to make it easier
--- to process by the type checker and the code generator.  
+-- to process by the semantic analyser, type checker and the code generator.  
 
  -- TODO: 
  -- convert all functions into lambda abstractions
@@ -11,10 +11,48 @@ module Backend.ASTConverter where
  -- introduce De Bruijn indices for bound variables
 
 import Frontend.LambdaQ.Par ( myLexer, pProgram )
+import Frontend.LambdaQ.Print ( printTree )
+import qualified Frontend.LambdaQ.Abs as GeneratedAbstractSyntax
 
-parseCode str = case pProgram (myLexer str) of
-    Left str -> -- todo
-    Right str -> -- todo
+data Type =
+    TypeBit |
+    TypeQbit |
+    TypeUnit |
+    TypeExp Type |
+    TypeTensrs Type Integer |
+    TypeTensr Type Type |
+    TypeFunc Type Type
+  deriving (Eq, Ord, Show, Read)
 
+data Function = Func String Type Term
+type Program = [Function]
 
+translateType :: GeneratedAbstractSyntax.Type -> Type
+translateType GeneratedAbstractSyntax.TypeBit   = TypeBit
+translateType GeneratedAbstractSyntax.TypeQbit  = TypeQbit
+translateType GeneratedAbstractSyntax.TypeUnit  = TypeUnit
+-- todo: finish
+
+reverseTranslateType :: Type -> GeneratedAbstractSyntax.Type
+reverseTranslateType TypeBit  = GeneratedAbstractSyntax.TypeBit
+reverseTranslateType TypeQbit = GeneratedAbstractSyntax.TypeQbit
+reverseTranslateType TypeUnit = GeneratedAbstractSyntax.TypeUnit
+-- todo: finish
+
+parse :: String -> Program
+parse str = case pProgram (myLexer str) of
+    Left str -> undefined
+    Right str -> undefined
+
+astToIast :: GeneratedAbstractSyntax.Program -> Program
+astToIast = undefined
+
+iastToAst ::  Program -> GeneratedAbstractSyntax.Program
+iastToAst = undefined
+
+parseAndPrintTreeFromString :: String -> String
+parseAndPrintTreeFromString = printTree . iastToAst . parse
+
+parseAndPrintTreeFromFile :: FilePath -> IO String
+parseAndPrintTreeFromFile path = parseAndPrintTreeFromString <$> readFile path
 
--- src/Backend/SemanticAnalyser.hs
+++ src/Backend/SemanticAnalyser.hs
@@ -1,9 +1,23 @@
-  -- TODO:
-  -- function names are unique
-  -- no duplicate function declaration exists
-  -- function signatures and definitions have matching names
-  -- the number of function arguments match the arguments in the signature
-  -- control qubits for controlled gates are different from target qubit
 
 module Backend.SemanticAnalyer where
 
+data SemanticError =
+    DuplicatedFunName String  |                          -- function names must be unique
+    DuplicateFunDeclaration String |                     -- there should be no duplicated function declaration
+    MismatchedFunDefinitionAndDeclaration String |       -- function signature in declaration should match the signature of function in definition
+    IncorrectNumberOfFunArguments String |               -- number of function arguments for a function call does not exceed number of arguments in signature
+    ControlQbitsNotDistinct String |                     -- control qubits for any given gate must be distinct
+    ControlAndTargetQbitsNotDistinct String |            -- for a controlled gate the control and target qubits must be distinct
+    UnknownGate                                          -- gate names should be recognized as belonging to the set of supported gates
+
+instance Show SemanticError where
+    show (DuplicatedFunName err) = "???"
+    show (DuplicateFunDeclaration err) = "???"  
+    show (MismatchedFunDefinitionAndDeclaration err) = "???"
+    show (IncorrectNumberOfFunArguments err) = "???"
+    show (ControlQbitsNotDistinct err) = "???"
+    show (ControlAndTargetQbitsNotDistinct err) = "???"
+    show (UnknownGate err) = "???"
+
+--performSemanticAnalysis :: Program -> Either SemanticError ()
+--performSemanticAnalysis program = ()
