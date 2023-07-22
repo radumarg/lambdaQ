1b07ea7279e8f5a6fab037e0b47cec3c47367eb8
Radu Marginean <radu.marg@gmail.com>
2023-07-22 18:22:15 +0300

    Comments update.

--- src/Backend/CodeGenerator.hs
+++ src/Backend/CodeGenerator.hs
@@ -1,4 +1,4 @@
--- Here the intermediate abstract syntax tree is evaluated and OpenQASM programs will be generated.
+-- Here the (annotated??) intermediate abstract syntax tree is evaluated and OpenQASM programs will be generated.
 -- Language lambdaQ implements classical control and call by value formal semantics.
 
 module Backend.CodeGenerator where
