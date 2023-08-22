865d97bbc6e75f77a351c902ddb6012cd0e4cb4d
Radu Marginean <radu.marg@gmail.com>
2023-08-22 20:02:22 +0300

    Fixing the Grover's algorithm.

--- src/Backend/ASTConverter.hs
+++ src/Backend/ASTConverter.hs
@@ -1,15 +1,11 @@
 -- A representation of a lambdaQ program in the shape of an abstract syntax tree
 -- will be generated here. Subsequently the abstract syntax tree is converted
 -- to an intermediate abstract syntax tree with a simpler syntax to make it easier
--- to process by the semantic analyser, type checker and the code generator:  
---   *  functions to be be converted to lambda abstractions
---   *  BNFC generated AST terms to be converted into an intermediate abstract syntax tree 
---   *  introduce De Bruijn indices for bound variables
+-- to process by the semantic analyser, type checker and the code generator.
 
 module Backend.ASTConverter where
 
-import Backend.IAST ( Function( Func ), Program, mapFunction, reverseMapFunction )
-import qualified Data.Map as Map
+import Backend.IAST (Program, mapFunction, reverseMapFunction )
 import Frontend.LambdaQ.Par ( myLexer, pProgram )
 import Frontend.LambdaQ.Print ( printTree )
 import qualified Frontend.LambdaQ.Abs as GenAbSyntax
@@ -21,14 +17,13 @@ parse str = case pProgram (myLexer str) of
    Right p -> mapProgram p
 
 mapProgram :: GenAbSyntax.Program -> Program
-mapProgram (GenAbSyntax.ProgDef fs)  = map mapFunction fs
+mapProgram (GenAbSyntax.ProgDef funs) = map mapFunction funs
 
-reverseMapProgram ::  Program -> GenAbSyntax.Program
+reverseMapProgram :: Program -> GenAbSyntax.Program
 reverseMapProgram = GenAbSyntax.ProgDef . map reverseMapFunction
 
 parseAndPrintTreeFromString :: String -> String
-parseAndPrintTreeFromString str = 
-  case pProgram (myLexer str) of
+parseAndPrintTreeFromString str = case pProgram (myLexer str) of
     Left str -> errorWithoutStackTrace str
     Right prog -> printTree prog
 
