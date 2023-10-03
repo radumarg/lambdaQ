0cf4e0685a28968a15d262a9790e45cca581c0a2
Radu Marginean <radu.marg@gmail.com>
2023-10-03 19:13:21 +0300

    Starting work on type checker.

--- src/Backend/TypeChecker.hs
+++ src/Backend/TypeChecker.hs
@@ -5,3 +5,29 @@
 -- Type checker will return an annotated syntax tree (??) - to be determined ..
 module Backend.TypeChecker where
 
+import Backend.ASTConverter (parse)
+import Backend.IAST (Program, Term, Type)
+import Data.Map (Map)
+import Data.Set (Set)
+
+typecheckProgram :: Program -> Either TypeError ()
+typecheckProgram program = undefined
+
+typecheckString :: String -> Either TypeError ()
+typecheckString = typecheckProgram . parse
+
+data TypeErrorInstance =
+    NotAFunction Type             |
+    FunctionNotInScope String     |
+    TypeMismatch Type Type        |
+    NotAProductType Type          | 
+    NotALinearType String         |
+    NotALinearTerm Term Type      |
+    NoCommonSupertype Type Type
+  deriving (Eq, Ord, Show, Read)
+
+data TypeError = TypeError String TypeErrorInstance
+  deriving (Eq, Ord, Show, Read)
+
+type LinearEnvironment = Set String
+type TopEnvironment = Map String Type
