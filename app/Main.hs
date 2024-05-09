21f4873e9ece362fd22e359fc849d84d0646a2a9
Radu Marginean <radu.marg@gmail.com>
2024-05-09 18:15:16 +0300

    Beautify code in main module.

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,28 @@
 import System.Environment ( getArgs )  
 import Control.Monad.Except ( runExceptT)
 import CompilationEngine (Exec, readTheFile, parseProgram, semanticAnalysis, convertAstToIast, typeCheck, generateCode)
-import System.IO
+import System.IO ( hClose, openFile, IOMode(ReadMode) )
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if length args == 1 && last3Elements (head args) == ".lq"
+    then
+      do
+        let filePath = head args
+        runExceptT (compile filePath) >>= \case
+          Left err -> putStrLn $ "Compilation error: " ++ show err ++ "!"
+          Right code -> 
+            do
+              handle <- openFile outputFile ReadMode
+              putStr code
+              hClose handle
+            where
+              outputFile = init (init filePath) ++ "qasm"
+      else
+        putStrLn "Please supply one argument: a path ponting to a lambdaQ program file!"
+  where
+    last3Elements = lastNElements 3
 
 compile :: FilePath -> Exec String
 compile filePath = readTheFile filePath
@@ -13,26 +34,6 @@ compile filePath = readTheFile filePath
                           >>= typeCheck
                           >>= generateCode
 
-lastN :: Int -> [a] -> [a]
-lastN n xs = drop (length xs - n) xs
+lastNElements :: Int -> [a] -> [a]
+lastNElements n xs = drop (length xs - n) xs
 
-main :: IO ()
-main = do
-  args <- getArgs
-  if length args == 1 && last3 (head args) == ".lq"
-    then
-      do
-        let filePath = head args
-        runExceptT (compile filePath) >>= \case
-          Left err -> putStrLn $ "Error: " ++ show err ++ "!"
-          Right code -> 
-            do
-              handle <- openFile outputFile ReadMode
-              putStr code
-              hClose handle
-            where
-              outputFile = init (init filePath) ++ "qasm"
-      else
-        putStrLn "Please supply one argument: a path to a lambdaQ program file!"
-  where
-    last3 = lastN 3
