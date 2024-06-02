5b8fb77fa4d8b5cade927c9c766319e2dbcc0b96
Radu Marginean <radu.marg@gmail.com>
2024-06-02 13:13:00 +0300

    Adding more test for lexer and parser.

--- test/Frontend/ParserSpec.hs
+++ test/Frontend/ParserSpec.hs
@@ -15,13 +15,13 @@ runParser :: FilePath -> Exec GeneratedAbstractSyntax.Program
 runParser filePath = readFileContents filePath
                               >>= parseProgram
 
-testParserReturnOK :: FilePath -> IO String
-testParserReturnOK filePath = runExceptT (runParser filePath)  >>= \case
+testParserReturnsOK :: FilePath -> IO String
+testParserReturnsOK filePath = runExceptT (runParser filePath)  >>= \case
                 Left err -> return (show err)
                 Right _ -> return "OK"
 
-testParserReturnTree :: FilePath -> IO String
-testParserReturnTree filePath = runExceptT (runParser filePath)  >>= \case
+testParserReturnsTree :: FilePath -> IO String
+testParserReturnsTree filePath = runExceptT (runParser filePath)  >>= \case
                 Left err -> return (show err)
                 Right program -> return (printTree program)
 
@@ -39,77 +39,140 @@ spec =  do
 
     Test.Hspec.context "when provided with a valid coinflip program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example00-CoinFlip.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example00-CoinFlip.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid Deutsch algorithm program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example01-deutschAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example01-deutschAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid Deutsch Jozsa algorithm program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example02-deutschJozsaAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example02-deutschJozsaAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid phase kick back program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example03-phaseKickBack.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example03-phaseKickBack.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid Bernstein Vazirani algorithm program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example04-bernsteinVaziraniAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example04-bernsteinVaziraniAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid Simon algorithm program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example05-simonAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example05-simonAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid teleportation protocol program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example06-teleportationProtocol.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example06-teleportationProtocol.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid four qubit adder program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example07-fourQubitAdder.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example07-fourQubitAdder.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid Grover algorithm program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example08-groverAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example08-groverAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid quantum phase estimation program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example09-quantumPhaseEstimation.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example09-quantumPhaseEstimation.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid Shor algorithm program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example10-shorAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example10-shorAlgorithm.lq" `Test.Hspec.shouldReturn` "OK"
 
     Test.Hspec.context "when provided with a valid QFT program" $ do
       Test.Hspec.it "returns no error" $ do
-        testParserReturnOK "test/programs/examples/example11-qft.lq" `Test.Hspec.shouldReturn` "OK"
+        testParserReturnsOK "test/programs/examples/example11-qft.lq" `Test.Hspec.shouldReturn` "OK"
 
   Test.Hspec.describe "Testing Lexer and Parser on various made up code fragments:" $ do
 
   -- EXPECT NO ERRORS --
 
-    Test.Hspec.context "when provided with a program containing '$' and term application" $ do
-      Test.Hspec.it "returns a parsed abstract syntax tree containing: fun = (a $ b) c" $ do
-        result <- testParserReturnTree "test/programs/good/check-terms-precedence/dollar_less_than_apply_1.lq"
-        result `Test.Hspec.shouldSatisfy` (\str -> "fun = (a $ b) c" `isInfixOf` str)
+    Test.Hspec.context "when provided with a program which verifies that '$' operator is right associative" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/dollar_is_right_associative_1.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ b $ c" `isInfixOf` str)
 
-    Test.Hspec.context "when provided with a program containing '$' and term application" $ do
-      Test.Hspec.it "returns a parsed abstract syntax tree containing: fun = a $ b c" $ do
-        result <- testParserReturnTree "test/programs/good/check-terms-precedence/dollar_less_than_apply_2.lq"
-        result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ b c" `isInfixOf` str)
+    Test.Hspec.context "when provided with a program which verifies that '$' operator is right associative" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/dollar_is_right_associative_2.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = (a $ b) $ c" `isInfixOf` str)
 
-    Test.Hspec.context "when provided with a program containing '$' follwed by an if else term" $ do
-      Test.Hspec.it "returns a parsed abstract syntax tree containing: fun = a $ if c then t else f" $ do
-        result <- testParserReturnTree "test/programs/good/check-terms-precedence/dollar_less_than_if_else.lq"
+    Test.Hspec.context "when provided with a program containing '$' operator and an if else term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/if_else_stronger_than_dollar_1.lq"
         result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ if c then t else f" `isInfixOf` str)
 
-    Test.Hspec.context "when provided with a program containing '$' follwed by an let term" $ do
-      Test.Hspec.it "returns a parsed abstract syntax tree containing: fun = a $ let { var = term } in term" $ do
-        result <- testParserReturnTree "test/programs/good/check-terms-precedence/dollar_less_than_let.lq"
+    Test.Hspec.context "when provided with a program containing '$' operator and an if else term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/if_else_stronger_than_dollar_2.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = if c then t else f $ a" `isInfixOf` str)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and an let term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_stronger_than_dollar_1.lq"
         result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ let { var = term } in term'" `isInfixOf` trimNewLines str)
 
+    Test.Hspec.context "when provided with a program containing '$' operator and let term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_stronger_than_dollar_2.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = let { var = term } in term' $ a" `isInfixOf` trimNewLines str)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and a let multiple term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_multiple_stronger_than_dollar_1.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ let { (var1, var2) = term } in term'" `isInfixOf` trimNewLines str)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and a let multiple term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_multiple_stronger_than_dollar_2.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = let { (var1, var2) = term } in term' $ a" `isInfixOf` trimNewLines str)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and an let sugar term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_sugar_stronger_than_dollar_1.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ var <- term; term'" `isInfixOf` trimNewLines str)
+        
+    Test.Hspec.context "when provided with a program containing '$' operator and let sugar term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_sugar_stronger_than_dollar_2.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = var <- term; term' $ a" `isInfixOf` trimNewLines str)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and a let multiple sugar term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_sugar_multiple_stronger_than_dollar_1.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ var1, var2 <- term; term'" `isInfixOf` trimNewLines str)
+        
+    Test.Hspec.context "when provided with a program containing '$' operator and a let sugar multiple term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/let_sugar_multiple_stronger_than_dollar_2.lq"
+        result `Test.Hspec.shouldSatisfy` (\str -> "fun = var1, var2 <- term; term' $ a" `isInfixOf` trimNewLines str)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and a case term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/case_stronger_than_dollar_1.lq"
+        --result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ var1, var2 <- term; term'" `isInfixOf` trimNewLines str)
+        print (trimNewLines result)
+
+    Test.Hspec.context "when provided with a program containing '$' operator and a case term" $ do
+      Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+        result <- testParserReturnsTree "test/programs/good/check-terms-precedence/case_stronger_than_dollar_2.lq"
+        --result `Test.Hspec.shouldSatisfy` (\str -> "fun = var1, var2 <- term; term' $ a" `isInfixOf` trimNewLines str)
+        print (trimNewLines result)
+
+    -- print (trimNewLines result)
+    -- Test.Hspec.context "when provided with a program containing '$' operator and term application" $ do
+    --   Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+    --     result <- testParserReturnsTree "test/programs/good/check-terms-precedence/dollar_less_than_apply_1.lq"
+    --     result `Test.Hspec.shouldSatisfy` (\str -> "fun = (a $ b) c" `isInfixOf` str)
+
+    -- Test.Hspec.context "when provided with a program containing '$' operator and term application" $ do
+    --   Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+    --     result <- testParserReturnsTree "test/programs/good/check-terms-precedence/dollar_less_than_apply_2.lq"
+    --     result `Test.Hspec.shouldSatisfy` (\str -> "fun = a $ b c" `isInfixOf` str)
+
     -- Test.Hspec.context "when provided with a valid coinflip program" $ do
-    --   Test.Hspec.it "returns a parsed abstract syntax tree containing: if a then b else c d" $ do
-    --     result <- testParserReturnTree "test/programs/good/check-terms-precedence/if_else.lq"
+    --   Test.Hspec.it "returns a parsed abstract syntax tree with redundant paranthesis removed" $ do
+    --     result <- testParserReturnsTree "test/programs/good/check-terms-precedence/if_else.lq"
     --     result `Test.Hspec.shouldSatisfy` (\str -> "if a then b else c d" `isInfixOf` str)
---
+++ test/programs/good/check-terms-precedence/test.hs
@@ -0,0 +1,6 @@
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    
